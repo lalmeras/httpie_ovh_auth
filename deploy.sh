47eef171f98d071e23dae9348b800c0c1b96cc26
Laurent Almeras <laurent.almeras@kobalt.fr>
2021-03-25 00:36:20 +0100

    rework travis conf
    deploy stage is triggered on all branches, but twine is inhibited.

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
 #! /bin/bash
 
+# fail fast
+set -e
+
 GPG_USER="httpieovhauth@gmail.com"
 GPG_OPTS="--pinentry-mode loopback --passphrase-fd 0"
+
+# import signing key
 echo "${gpg_passphrase}" | \
   gpg $GPG_OPTS --import httpieovhauth.gpg
+
+# build
 python setup.py sdist bdist_wheel --python-tag py3
+
+# sign
 for file in dist/*; do
   echo "$gpg_passphrase" | \
-    gpg --detach-sign -u "$GPG_USER" -o "$file.asc" "$file";
-done;
-TWINE_PASSWORD="${pypi_token}" twine upload -u __token__ dist/*
+    gpg $GPG_OPTS --detach-sign -u "$GPG_USER" -o "$file.asc" "$file" >/dev/null 2>&1
+done
+echo "dist/ files"
+ls dist/
+
+# publish
+#TWINE_PASSWORD="${pypi_token}" twine upload -u __token__ dist/*
