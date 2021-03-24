051ac71fafffa495d16b252acf9dadb360ad7a75
Laurent Almeras <laurent.almeras@kobalt.fr>
2021-03-24 01:23:25 +0100

    fix travis conf file

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+GPG_USER="httpieovhauth@gmail.com"
+GPG_OPTS="--pinentry-mode loopback --passphrase-fd 0"
+echo "${gpg_passphrase}" | \
+  gpg $GPG_OPTS --import httpieovhauth.gpg
+python setup.py sdist bdist_wheel --python-tag py3
+for file in dist/*; do
+  echo "$gpg_passphrase" | \
+    gpg --detach-sign -u "$GPG_USER" -o "$file.asc" "$file";
+done;
+TWINE_PASSWORD="${pypi_token}" twine upload dist/*
